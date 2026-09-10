730db7b6299bf70e17555bcfe68d1bed7efa1ee5
vincemelegrito55-coder <vincemelegrito55@gmail.com>
2026-09-10 15:44:33 +0800

    Lab Activity 5: CRUD with Authentication

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -43,4 +43,5 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 $config['middlewares'] = [
 	'student_access' => load_class('StudentMiddleware', 'middlewares'),
+	'auth'            => load_class('AuthMiddleware', 'middlewares'),
 ];
